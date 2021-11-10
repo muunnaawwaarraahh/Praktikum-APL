5384328d06d78c4086485575311905728e1b9e37
muunnaawwaarraahh <82081134+muunnaawwaarraahh@users.noreply.github.com>
2021-11-10 23:21:15 +0700

    Update Main.java

--- Adapter Pattern Kucing/Main.java
+++ Adapter Pattern Kucing/Main.java
@@ -1,9 +1,11 @@
 class Main {
     public static void main (String[] args) {
         Kucing kucing = new Meow();
+        System.out.println("Suara asli kucing ... ");
         kucing.meow();
 
-        MeowAdapter meowAdapter = new MeowAdapter(kucing);
+        Bebek meowAdapter = new MeowAdapter(kucing);
+        System.out.println("Suara bebek kwekkk kweeekk!! ");
         meowAdapter.kwek();
     }
 }
